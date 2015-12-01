4c6dfeafcc37032aeff4bd54ee6e2cfd2081cf96
pesetskyps <pavel_pesetskiy@epam.com>
2015-12-01 16:52:46 +0300

    init

---
+++ CleanNugetSharp/GetPackageDependencies.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NuGet;
+
+namespace CleanNugetSharp
+{
+  public class GetPackageDependencies
+  {
+    IPackageRepository repo;
+
+    public GetPackageDependencies(IPackageRepository repository)
+    {
+      repo = repository;
+    }
+    public IPackage GetPackageDependenciesRecursive(IPackage package, ref List<PackageDependency> dependencies)
+    {
+
+      var packageDependencySets = package.DependencySets;
+      if (!packageDependencySets.Any())
+      {
+        return package;
+      }
+      foreach (var packageDependencySet in packageDependencySets)
+      {
+        foreach (var dependency in packageDependencySet.Dependencies)
+        {
+          //SemanticVersion ver = new SemanticVersion(dependency.VersionSpec.MaxVersion);
+          var dependecyPackage = repo.ResolveDependency(dependency, true, true);
+          if (dependecyPackage != null)
+          {
+            return GetPackageDependenciesRecursive(dependecyPackage, ref dependencies);
+          }
+          else
+          {
+            throw new Exception("Dependency not found");
+          }
+        }
+      }
+      return package;
+    }
+  }
+}
---
+++ CleanNugetSharp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Xml;
+using NuGet;
+
+namespace CleanNugetSharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      List<string> DatasourcePackages = new List<string>();
+      string fullProjectPath = @"d:\Dev\Git\CA\Cloud.Apps.Accounting.Web\src\Cloud.UI.Accounting\Cloud.UI.Accounting.csproj";
+      XmlDocument projDefinition = new XmlDocument();
+      projDefinition.Load(fullProjectPath);
+      XmlNamespaceManager nsmgr = new XmlNamespaceManager(projDefinition.NameTable);
+      nsmgr.AddNamespace("bk", "http://schemas.microsoft.com/developer/msbuild/2003");
+
+      XmlNodeList dataSourcesReferences = projDefinition.SelectNodes("//bk:Reference[contains(@Include, 'Plex.DataSources') and bk:Private]", nsmgr);
+
+      if (dataSourcesReferences != null)
+        foreach (XmlElement reference in dataSourcesReferences)
+        {
+          XmlNode referenceNode = reference.SelectSingleNode("bk:HintPath", nsmgr);
+          if (referenceNode != null)
+          {
+            var packagePath = referenceNode.InnerText;
+            const string packagesMask = @"..\lib\";
+            var f = packagePath.Substring(packagePath.IndexOf(packagesMask, StringComparison.Ordinal) + packagesMask.Length);
+            var ff = f.Replace(".dll", null);
+            ff += ".Dev";
+            DatasourcePackages.Add(ff);
+          }
+          foreach (var datasourcePackage in DatasourcePackages)
+          {
+            IPackageRepository repo = PackageRepositoryFactory.Default.CreateRepository("http://localhost:5555/nuget");
+           
+            var pack = repo.FindPackagesById(datasourcePackage).Last();
+          
+            var packageDependencySets = pack.DependencySets;
+            foreach (var packageDependencySet in packageDependencySets)
+            {
+              foreach (var dependency in packageDependencySet.Dependencies)
+              {
+                IPackage package;
+                //SemanticVersion ver = new SemanticVersion(dependency.VersionSpec.MaxVersion);
+                var dep = repo.ResolveDependency(dependency,true,true);
+                if (repo.TryFindPackage(dependency.Id, dependency.VersionSpec.MaxVersion, out package))
+                {
+                  
+                }
+              }
+            }
+            Console.WriteLine(pack);
+
+          }
+          //Console.WriteLine(f);
+        }
+      Console.ReadLine();
+    }
+  }
+}
